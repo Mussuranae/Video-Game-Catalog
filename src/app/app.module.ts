92c031f10b4b4a95a7c634b665dd87cd420e8d54
Mussuranae <mandonnetdev@outlook.com>
2019-07-09 18:02:16 +0200

    First component OK, try to use Angular !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MzButtonModule, MzInputModule } from 'ngx-materialize';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoGameComponent } from './video-game/video-game.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoGameComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MzButtonModule,
+    MzInputModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
