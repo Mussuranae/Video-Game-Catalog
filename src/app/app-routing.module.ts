40bf7a3748ab356e7f2e123a5f86718b63f21365
Mussuranae <mandonnetdev@outlook.com>
2019-07-12 15:00:02 +0200

    button et mise en page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VideoGameComponent } from './video-game/video-game.component';
 import { CreateGameComponent } from './create-game/create-game.component';
+import { DetailsGameComponent } from './details-game/details-game.component';
 
 const routes: Routes = [
     { path: '', component: VideoGameComponent },
-    { path: 'new', component: CreateGameComponent }
+    { path: 'new', component: CreateGameComponent },
+    { path: 'details', component: DetailsGameComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatCardModule, MatGridListModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule, MatFormFieldModule, MatInputModule, MatCardModule, MatGridListModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoGameComponent } from './video-game/video-game.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
 import { CreateGameComponent } from './create-game/create-game.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DetailsGameComponent } from './details-game/details-game.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoGameComponent,
-    NavbarComponent,
     CreateGameComponent,
     DetailsGameComponent,
   ],
@@ -41,7 +42,8 @@ import { DetailsGameComponent } from './details-game/details-game.component';
         environment.production ? [] : AkitaNgDevtools.forRoot(),
         AkitaNgRouterStoreModule.forRoot(),
         ReactiveFormsModule,
-        MatInputModule
+        MatInputModule,
+        MatIconModule
     ],
   providers: [],
   bootstrap: [AppComponent]
