1c6087fc4b355a631398cc317f5df55e2a7392a8
Mussuranae <mandonnetdev@outlook.com>
2019-07-16 12:09:51 +0200

    réorganisation des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { DetailsGameComponent } from './details-game/details-game.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-    { path: '', component: GameListComponent },
-    { path: 'new', component: CreateGameComponent },
     { path: 'edit/:id', component: CreateGameComponent},
     { path: 'game/:id', component: DetailsGameComponent},
-    { path: 'cart', component: CartComponent}
+    { path: 'new', component: CreateGameComponent },
+    { path: 'cart', component: CartComponent},
+    { path: '', component: GameListComponent}
 ];
 
 @NgModule({
